a45262b3b3e55e020a7c03bf47f9b039d49d69ab
markmdoza <marklmdoza@gmail.com>
2024-07-25 11:36:15 -0400

    initial commit

---
+++ WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Mark Mendoza on 2024-07-25.
+//
+
+import Foundation
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mark Mendoza on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
